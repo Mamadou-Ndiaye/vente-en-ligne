f9a66582b8db0887e2bcb203151d26713553c6b9
Mamadou_Ndiaye <ndiamamadou@gmail.com>
2021-04-17 17:13:43 +0200

    Add JDL model shopping Generate entities from JDL Model Shopping

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,31 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'category',
+        data: { pageTitle: 'jhipsterApp.category.home.title' },
+        loadChildren: () => import('./category/category.module').then(m => m.CategoryModule),
+      },
+      {
+        path: 'product',
+        data: { pageTitle: 'jhipsterApp.product.home.title' },
+        loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+      },
+      {
+        path: 'customer',
+        data: { pageTitle: 'jhipsterApp.customer.home.title' },
+        loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),
+      },
+      {
+        path: 'address',
+        data: { pageTitle: 'jhipsterApp.address.home.title' },
+        loadChildren: () => import('./address/address.module').then(m => m.AddressModule),
+      },
+      {
+        path: 'wish-list',
+        data: { pageTitle: 'jhipsterApp.wishList.home.title' },
+        loadChildren: () => import('./wish-list/wish-list.module').then(m => m.WishListModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
